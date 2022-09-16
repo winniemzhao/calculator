b1316abfef8c1a250d6127333cc24d35cde49c97
Winnie Zhao <winniemaizhao@gmail.com>
2022-09-16 00:13:50 -0400

    add dynamic functionality to clear and backspace buttons

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello from js")
+
+// selecting result
+const result = document.querySelector("#resultValue");
+// let resultValue = Number.parseInt(result.innerText);
+console.log(result);
+
+// clear button
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", function () {
+  // resultValue = 0;
+  result.innerText = 0;
+})
+
+// backspace button
+const backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", function (event) {
+  if (result.innerText.length > 1) {
+    result.innerText = result.innerText.slice(0, -1);
+  } else {
+    result.innerText = 0;
+  }
+});

cbafcd5dabaa065994a3c6c42acd74cc1dc8d2f7
Winnie Zhao <winniemaizhao@gmail.com>
2022-09-16 00:31:57 -0400

    replace fontawesome icons with unimcode characters

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // selecting result
 const result = document.querySelector("#resultValue");
 // let resultValue = Number.parseInt(result.innerText);
-console.log(result);
 
 // clear button
 const clear = document.querySelector(".clear");
@@ -13,10 +12,31 @@ clear.addEventListener("click", function () {
 
 // backspace button
 const backspace = document.querySelector(".backspace");
-backspace.addEventListener("click", function (event) {
+backspace.addEventListener("click", function () {
   if (result.innerText.length > 1) {
     result.innerText = result.innerText.slice(0, -1);
   } else {
     result.innerText = 0;
   }
+});
+
+// number buttons
+const numbers = document.querySelectorAll(".number button");
+numbers.forEach((number) => {
+  number.addEventListener("click", function () {
+    if (result.innerText === "0") {
+      result.innerText = number.innerText
+    } else {
+      result.innerText = result.innerText + number.innerText;
+    }
+  });
+});
+
+// operator buttons
+
+const operators = document.querySelectorAll(".operator button");
+operators.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    console.log(operator.classList);
+  });
 });
